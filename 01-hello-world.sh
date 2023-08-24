ded82534e386382ef1cf457206a75c474c582d12
nagabob <kadiayalanaga83@gmail.com>
2023-08-24 06:29:37 +0530

    Fist programme

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -2,3 +2,8 @@
 
 echo "Hello world"
 echo "I am learning shell scripting and git"
+
+echo "Ramesh: Hi, Good morning suresh"
+echo "Suresh : Good morning ramesh"
+echo "Ramesh: How are you doing today"
+echo "Suresh : doing good, thank you. How about you"

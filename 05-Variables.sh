f32852a8bfb8d63c36f76506d1ee5423616ba163
nagabob <kadiayalanaga83@gmail.com>
2023-08-24 11:30:54 +0530

    Pracitse

--- 05-Variables.sh
+++ 05-Variables.sh
@@ -5,3 +5,9 @@ echo "Please enter your username: "
 read USERNAME # this is the value entered by user at terminal
 
 echo "uesr entered is: $USERNAME"
+
+echo "Please enter your passowrd: "
+
+read PASSWORD # this is the value entered by user at terminal
+
+echo "uesr entered is: $PASSWORD"

83ef3ecf4bffb17cef46db1c132af2d084508b76
nagabob <kadiayalanaga83@gmail.com>
2023-08-24 06:34:40 +0530

    Fist programme

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -3,7 +3,11 @@
 echo "Hello world"
 echo "I am learning shell scripting and git"
 
-echo "Ramesh: Hi, Good morning suresh"
-echo "Suresh : Good morning ramesh"
-echo "Ramesh: How are you doing today"
-echo "Suresh : doing good, thank you. How about you"
+PERSON1=Ramesh
+PERSON2=SURESH
+
+
+echo "$PERSON1: Hi, Good morning $PERSON2"
+echo "$PERSON2 : Good morning $PERSON1"
+echo "$PERSON1: How are you doing today"
+echo "$PERSON2 : doing good, thank you. How about you"

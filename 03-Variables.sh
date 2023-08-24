ad36a84a6ccd4c8e85a7f8cef81274de41d9955d
nagabob <kadiayalanaga83@gmail.com>
2023-08-24 06:41:53 +0530

    Fist programme

--- 03-Variables.sh
+++ 03-Variables.sh
@@ -6,7 +6,7 @@ PERSON1=$1
 PERSON2=$2
 
 
-echo "$$1: Hi, Good morning $$2"
+echo "$1: Hi, Good morning $2"
 echo "$2 : Good morning $1"
 echo "$1: How are you doing today"
 echo "$2 : doing good, thank you. How about you"

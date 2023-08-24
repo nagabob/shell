e067b7a02c53e61d26a4541c72c232bd214d4eea
nagabob <kadiayalanaga83@gmail.com>
2023-08-24 06:39:47 +0530

    Fist programme

---
+++ 03-Variables.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# Inputs from command line
+
+PERSON1=$1
+PERSON2=$2
+
+
+echo "$PERSON1: Hi, Good morning $PERSON2"
+echo "$PERSON2 : Good morning $PERSON1"
+echo "$PERSON1: How are you doing today"
+echo "$PERSON2 : doing good, thank you. How about you"

864716e8b934dff139b8a8f8f84c1583b7101193
nagabob <kadiayalanaga83@gmail.com>
2023-08-25 11:41:08 +0530

    Practise

--- 11-loops.sh
+++ 11-loops.sh
@@ -3,4 +3,6 @@
 for i in {1..100}
 do
     echo $i
+    echo "Iteration number: $i"
 done
+echo "completed"
---
+++ 13-Task.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DATE=$(date +%F)
+SCRIPTNAME=$0
+LOGFILE=/tmp/$SCRIPTNAME-/$DATE.log
+USERID=$(id -u)
+
+if ($USERID -ne 0)
+then
+    echo "Run the script with root access"
+    exit 1
+fi
+
+for i in $@
+do
+    yum install $i -y
+    VALIDATE 
+done
+
+VALIDATE(){
+    if ($1 -ne 0)
+    then
+        echo "$i Installation is success"
+    else
+        echo "$i Installation is failure"
+    fi
+}

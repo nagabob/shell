df2fadee1636deb0e693efda98bcbf72e4aecb5a
nagabob <kadiayalanaga83@gmail.com>
2023-08-25 12:49:25 +0530

    Practise

--- 13-Task.sh
+++ 13-Task.sh
@@ -6,6 +6,7 @@ LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
 
 if [ $USERID -ne 0 ]
@@ -29,7 +30,7 @@ PACKAGE(){
         yum install $i -y >>$LOGFILE
         VALIDATE $? $i
     else
-        echo -e "$i $G already installed$N"
+        echo -e "$i $Y already installed$N"
     fi
 }
 for i in $@

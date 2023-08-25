b5b8887e70ad1cd5af2652a82555b7b4fa547899
nagabob <kadiayalanaga83@gmail.com>
2023-08-25 05:48:44 +0530

    Practise

--- 10-functions.sh
+++ 10-functions.sh
@@ -5,14 +5,19 @@ SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 USERID=$(id -u)
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 VALIDATE(){
     #$1 --> it will receive the argument1
 if [ $1 -ne 0 ]
 then
-    echo "$2 Installation.... Failure"
+    echo-e "$2 Installation....$R Failure $N"
     exit 1
 else
-    echo "$2 Installation.... success"
+    echo "$2 Installation.... $G success $N"
 fi
 }
 
